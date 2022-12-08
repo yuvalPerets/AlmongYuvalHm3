ac7c64afe42f68b76cbd65e9302344dc9e7bf042
Uv_perets <yuval.perets@ruppin365.net>
2022-12-08 12:46:03 +0200

    commit1 , swtich places to correct test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -16,14 +16,14 @@ namespace UnitTest1
 			std :: string excpectedResult = "Error"; 
 			PRNParser* a = new PRNParser();
 			string result  = a->ParseLine("Wrong_Length_Of_Input");
-			Assert::AreEqual ( result , excpectedResult);
+			Assert::AreEqual (excpectedResult, result);
 		}
 		TEST_METHOD(Test_Done_input)
 		{
 			std::string excpectedResult1 = "Done";
 			PRNParser* a = new PRNParser();
 			string result = a->ParseLine("The House of Doctor Dee                 Ackroyd,Peter                 9780140171174");
-			Assert::AreEqual(result, excpectedResult1);
+			Assert::AreEqual(excpectedResult1 , result);
 		}
 	};
 }

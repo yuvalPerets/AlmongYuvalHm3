0d10048252a9a6663c86849cd9524009b5902fc1
Uv_perets <yuval.perets@ruppin365.net>
2022-12-08 13:59:13 +0200

    added 1 more unit test for last read

---
+++ UnitTestLastBook/UnitTestLastBook.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLastBook
+{
+	TEST_CLASS(UnitTestLastBook)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
